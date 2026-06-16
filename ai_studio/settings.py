dc7c193ffa9405f771e2c3003ba040bf6be722e5
Taha Shehzad <tahashehzad063@gmail.com>
2026-06-16 15:00:31 +0500

    Fix Railway deployment (gunicorn + CORS + settings)

--- ai_studio/settings.py
+++ ai_studio/settings.py
@@ -41,10 +41,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -150,11 +150,11 @@ SIMPLE_JWT = {
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:3000,http://localhost:8000,http://127.0.0.1:3000'
+    'http://localhost:3000,http://localhost:8000,http://localhost:8081,http://127.0.0.1:3000,http://127.0.0.1:8081,https://auth-production-f0ea.up.railway.app'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 # Email Configuration (Gmail SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
